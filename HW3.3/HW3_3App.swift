f2687ccdc62dbed8f08540ac7c06283317a390cd
Alexander <a-l-e-k-s@yandex.ru>
2021-04-02 13:04:47 +0300

    Initial Commit

---
+++ HW3.3/HW3_3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_3App.swift
+//  HW3.3
+//
+//  Created by Alexander Konovalov on 02.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW3_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
